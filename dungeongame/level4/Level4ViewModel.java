45b9765600c050aa51909248e2697b35e42f9571
Muthupradeesh S <148619798+muthupradeeshs@users.noreply.github.com>
2023-12-23 15:34:35 +0530

    Update Level4ViewModel.java

--- dungeongame/level4/Level4ViewModel.java
+++ dungeongame/level4/Level4ViewModel.java
@@ -7,16 +7,16 @@ public class Level4ViewModel {
 		this.level4View = level4View;
 	}
 	public int getResult(int row,int column,int adventureRow,int adventureCol,int monsterRow,int monsterCol,int triggerRow,int triggerCol,int goldRow,int goldCol)
-	{   int result;
-		int monsterStep=Math.abs(adventureRow-goldRow) + Math.abs(adventureCol-goldCol);
-		int  adventureStep=Math.abs(monsterRow-goldRow) + Math.abs(monsterCol-goldCol);
+	{  
+		int adventureStep=Math.abs(adventureRow-goldRow) + Math.abs(adventureCol-goldCol);
+		int  monsterStep=Math.abs(monsterRow-goldRow) + Math.abs(monsterCol-goldCol);
 		int  triggerStep=Math.abs(triggerRow-goldRow) + Math.abs(triggerCol-goldCol);
 		int trigger2=Math.abs(adventureRow-triggerRow) + Math.abs(adventureCol-triggerCol);
-		if(monsterStep<=adventureStep)
+		if(monsterStep>=adventureStep)
 		{
 			return adventureStep;
 		}
-		else if(monsterStep>adventureStep && monsterStep<=triggerStep)
+		else if(monsterStep<adventureStep && monsterStep<=triggerStep)
 		{
 			return trigger2+triggerStep;
 		}
